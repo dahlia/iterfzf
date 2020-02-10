15daa04999209dc1445304e66881a8482d9f8416
Abraham Hinteregger <oerpli@outlook.com>
2020-02-10 23:01:46 +0100

    refactor
    early exit

--- iterfzf/__init__.py
+++ iterfzf/__init__.py
@@ -4,6 +4,7 @@ import errno
 import os.path
 import subprocess
 import sys
+from itertools import chain
 
 from pkg_resources import resource_exists, resource_filename
 
@@ -49,75 +50,80 @@ def iterfzf(
         cmd.append('--multi')
     if not mouse:
         cmd.append('--no-mouse')
-    if print_query:
-        cmd.append('--print-query')
     if query:
         cmd.append('--query=' + query)
     if preview:
         cmd.append('--preview=' + preview)
+    # always use --print-query: makes things simpler later
+    # as return values are always the same
+    cmd.append('--print-query')
+    query_results = _exec_cmd(cmd, iterable, encoding, executable)
+    query, results = query_results[0], query_results[1:]
+    if len(results) > 1 and results[0] is not None:
+        return_value = results if multi else results[0]
+    else:
+        return_value = None
+    if print_query:
+        return query, return_value
+    else:
+        return return_value
+
+
+def _exec_cmd(cmd, iterable, encoding, executable):
+    """Create subprocess with fzf and return its output to the caller.
+    Most of the work here is related to making sure that the encodings
+    in both directions are correct.
+    In either case returns a tuple where the first element is the query
+    which will be returned if query_result=True as well as the chosen items
+
+    Arguments:
+        cmd List[str] -- Command to be executed via subprocess
+        ... -- just passed through from function call above
+
+    Raises:
+        ValueError: If iterable contains unsuitable elements
+
+    Returns:
+        str,List[str] -- The query (search entered in fzf) + list of results
+    """
     encoding = encoding or sys.getdefaultencoding()
-    proc = None
-    stdin = None
-    byte = None
-    lf = u'\n'
-    cr = u'\r'
-    for line in iterable:
-        if byte is None:
-            byte = isinstance(line, bytes)
-            if byte:
-                lf = b'\n'
-                cr = b'\r'
-        elif isinstance(line, bytes) is not byte:
-            raise ValueError(
-                'element values must be all byte strings or all '
-                'unicode strings, not mixed of them: ' + repr(line)
-            )
-        if lf in line or cr in line:
-            raise ValueError(r"element values must not contain CR({1!r})/"
-                             r"LF({2!r}): {0!r}".format(line, cr, lf))
-        if proc is None:
-            proc = subprocess.Popen(
-                cmd,
-                stdin=subprocess.PIPE,
-                stdout=subprocess.PIPE,
-                stderr=None
-            )
-            stdin = proc.stdin
-        if not byte:
-            line = line.encode(encoding)
-        try:
-            stdin.write(line + b'\n')
-            stdin.flush()
-        except IOError as e:
-            if e.errno != errno.EPIPE and errno.EPIPE != 32:
-                raise
-            break
+    proc = subprocess.Popen(
+        cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=None
+    )
+    stdin = proc.stdin
+    iterator = iter(iterable)
+    first = next(iterator, None)
+    is_byte = None
+    if first is not None:
+        is_byte = isinstance(first, bytes)
+        lf = b'\n' if is_byte else '\n'
+        cr = b'\r' if is_byte else '\r'
+        for line in chain([first], iterator):
+            if isinstance(line, bytes) != is_byte:
+                raise ValueError(
+                    'element values must be all byte strings or all '
+                    'unicode strings, not mixed of them: ' + repr(line)
+                )
+            if lf in line or cr in line:
+                raise ValueError(
+                    r'element values must not contain CR({1!r})/'
+                    r'LF({2!r}): {0!r}'.format(line, cr, lf)
+                )
+            line_b = line if is_byte else line.encode(encoding)
+            try:
+                stdin.write(line_b + b'\n')
+                stdin.flush()
+            except IOError as e:
+                if e.errno != errno.EPIPE and errno.EPIPE != 32:
+                    raise
+                break
     if proc is None or proc.wait() not in [0, 1]:
-        if print_query:
-            return None, None
-        else:
-            return None
+        return None, None
     try:
         stdin.close()
     except IOError as e:
         if e.errno != errno.EPIPE and errno.EPIPE != 32:
             raise
     stdout = proc.stdout
-    decode = (lambda b: b) if byte else (lambda t: t.decode(encoding))
-    output = [decode(l.strip(b'\r\n\0')) for l in iter(stdout.readline, b'')]
-    if print_query:
-        try:
-            if multi:
-                return output[0], output[1:]
-            else:
-                return output[0], output[1]
-        except IndexError:
-            return output[0], None
-    else:
-        if multi:
-            return output
-        else:
-            try:
-                return output[0]
-            except IndexError:
-                return None
+    decode = (lambda b: b) if is_byte else (lambda t: t.decode(encoding))
+    return [decode(l.strip(b'\r\n\0')) for l in iter(stdout.readline, b'')]
