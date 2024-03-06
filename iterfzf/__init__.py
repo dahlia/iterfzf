cc1647aa08d0a046b2131d2c913356c1b1e0f0bc
Hong Minhee <hong@minhee.org>
2024-03-06 11:16:56 +0900

    Version bump & changelog for #34

--- iterfzf/__init__.py
+++ iterfzf/__init__.py
@@ -10,7 +10,7 @@ from typing import AnyStr, Iterable, Literal, Optional
 __all__ = '__fzf_version__', '__version__', 'BUNDLED_EXECUTABLE', 'iterfzf'
 
 __fzf_version__ = '0.44.0'
-__version__ = '1.1.' + __fzf_version__
+__version__ = '1.2.' + __fzf_version__
 
 POSIX_EXECUTABLE_NAME: Literal['fzf'] = 'fzf'
 WINDOWS_EXECUTABLE_NAME: Literal['fzf.exe'] = 'fzf.exe'
